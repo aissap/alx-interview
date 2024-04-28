4ffcf6c89a544b666d4e6dfa133d41ebca14de54
aissap <jesus.jesus.jesus.core@gmail.com>
2024-04-28 23:21:26 +0000

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,16 +9,16 @@ n (int): The number of rows of the triangle to generate.
 Returns:
 list of lists: List of lists of integers
 """
-	triangle = []
+  triangle = []
 
-	if n <= 0:
-		return triangle
+  if n <= 0:
+    return triangle
 
-	for i in range(n):
-		row = [1] * (i + 1)  # Initialize each row with 1s
-		if i > 1:
-			for j in range(1, i):
-				row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-		triangle.append(row)
+  for i in range(n):
+    row = [1] * (i + 1)  # Initialize each row with 1s
+    if i > 1:
+      for j in range(1, i):
+        row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+    triangle.append(row)
 
-	return triangle
+  return triangle
