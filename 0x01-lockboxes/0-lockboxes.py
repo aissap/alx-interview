b775384b5e942cd4ecf5250f828c6ea0d2a0feff
aissap <jesus.jesus.core@gmail.com>
2024-05-06 02:39:09 +0000

    documentation

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,17 +3,29 @@
 
 def canUnlockAll(boxes):
     """
-    Determine if all boxes can be opened."""
-  num_boxes = len(boxes)
-  visited = set()
-  visited.add(0)
-  box_queue = [0]
+    Determine if all boxes can be opened.
 
-  while box_queue:
-      current_box = box_queue.pop(0)
-      for key in boxes[current_box]:
-          if key < num_boxes and key not in visited:
-              visited.add(key)
-              box_queue.append(key)
+    Args:
+        boxes (list of lists): A list of lists representing boxes and their keys.
 
-  return len(visited) == num_boxes
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+    if not isinstance(boxes, list) or not all(isinstance(box, list) for box in boxes):
+        return False
+
+    num_boxes = len(boxes)
+    if num_boxes == 0:
+        return False
+
+    visited_boxes = set([0])
+    box_queue = [0]  # Queue for BFS traversal
+
+    while box_queue:
+        current_box = box_queue.pop(0)
+        for key in boxes[current_box]:
+            if 0 <= key < num_boxes and key not in visited_boxes:
+                visited_boxes.add(key)
+                box_queue.append(key)
+
+    return len(visited_boxes) == num_boxes
