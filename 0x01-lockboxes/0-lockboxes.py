3619b3eca758a2860258ca72def4db383707c11f
aissap <jesus.jesus.core@gmail.com>
2024-05-06 02:31:52 +0000

    documentation

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,24 +3,17 @@
 
 def canUnlockAll(boxes):
     """
-    Determine if all boxes can be opened.
+    Determine if all boxes can be opened."""
+  num_boxes = len(boxes)
+  visited = set()
+  visited.add(0)
+  box_queue = [0]
 
-    Args:
-        boxes: A list of lists representing boxes and their keys.
+  while box_queue:
+      current_box = box_queue.pop(0)
+      for key in boxes[current_box]:
+          if key < num_boxes and key not in visited:
+              visited.add(key)
+              box_queue.append(key)
 
-    Returns:
-        bool: True if all boxes can be opened, False otherwise.
-    """
-    num_boxes = len(boxes)
-    visited = set()
-    visited.add(0)
-    box_queue = [0]
-
-    while box_queue:
-        current_box = box_queue.pop(0)
-        for key in boxes[current_box]:
-            if key < num_boxes and key not in visited:
-                visited.add(key)
-                box_queue.append(key)
-
-    return len(visited) == num_boxes
+  return len(visited) == num_boxes
