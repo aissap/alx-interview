7e7250bcb407a0e3ad863517beaf9e41961dc0e0
aissap <jesus.jesus.core@gmail.com>
2024-07-04 16:43:43 +0200

    perime-game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+Prime Game module
+"""
+
+def is_prime(n):
+    """Check if a number is prime"""
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def get_primes_up_to(n):
+    """Get all prime numbers up to n"""
+    primes = []
+    for i in range(2, n + 1):
+        if is_prime(i):
+            primes.append(i)
+    return primes
+
+def isWinner(x, nums):
+    """Determine the winner."""
+    if x <= 0 or not nums:
+        return None
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        primes = get_primes_up_to(n)
+        moves = 0
+
+        while primes:
+            prime = primes.pop(0)
+            multiples = [i for i in primes if i % prime == 0]
+            primes = [i for i in primes if i % prime != 0]
+            moves += 1
+
+        if moves % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
